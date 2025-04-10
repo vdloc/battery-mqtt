d92414991d3a26959a42e0393d35c1ea71882773
vdloc <uvcudlco@gmail.com>
2025-04-10 23:09:27 +0700

    fix: rename import for getDeviceStatus to getDeviceStatusRoute in mqttRouter

--- server/router/mqttRouter.ts
+++ server/router/mqttRouter.ts
@@ -3,12 +3,12 @@ import requestRoute from "./mqtt/request"
 import getDevicesRoute from "./mqtt/getDevices"
 import getIntervalsRoute from "./mqtt/getIntervals"
 import getDeviceSetupChannelsRoute from "./mqtt/getDeviceSetupChannels"
-import getDeviceStatus from "./mqtt/getDeviceStatus"
+import getDeviceStatusRoute from "./mqtt/getDeviceStatus"
 
 const mqttRouter = router({
   request: requestRoute,
   getDevices: getDevicesRoute,
-  getDeviceStatus: getDeviceStatus,
+  getDeviceStatus: getDeviceStatusRoute,
   getIntervals: getIntervalsRoute,
   getDeviceSetupChannels: getDeviceSetupChannelsRoute,
 })
